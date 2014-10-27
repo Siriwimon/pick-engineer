bd47b1aa73b50b16f0fbc5c58d21c57c16499e13
Siriwimon <siriwimon@email.com>
2014-10-27 12:46:09 -0300

    add process in pick_engineer()

--- pick_engineer.rb
+++ pick_engineer.rb
@@ -1,4 +1,10 @@
 
 def pick_engineer()
-
+	engineers = Array.new()
+	File.readlines("Engineer.txt").each do |line| 
+		engineers.push(line)
+	end
+	puts engineers[rand(engineers.count)]	
 end
+
+pick_engineer()
